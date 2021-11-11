7129385915b38a8bd26af3b4ded0496429229e7e
Gencehan <gencehangurcam@gmail.com>
2021-11-11 18:33:47 +0100

    project init

--- js/main.js
+++ js/main.js
@@ -1,22 +1,60 @@
-let input = document.querySelector('input')
-let addBtn = document.querySelector('button')
-let toDos = document.querySelector('.toDos')
+const addButton = document.querySelector('.addButton');
+var input = document.querySelector('.input');
+const container = document.querySelector('.container'); 
 
-console.log(input);
-console.log(addBtn);
-console.log(toDos);
-addBtn.addEventListener('click', () =>{
-    let paragraph = document.createElement('p')
-    paragraph.innerText = input.value;
-    toDos.appendChild(paragraph);
-    input.value = '';
-    paragraph.addEventListener('click', () =>{
-        paragraph.style.textDecoration = "line-through"
-    })
-})
-addBtn.addEventListener('dblclick', () =>{
-    let paragraph = document.createElement('p')
-    paragraph.innerText = input.value;
-    toDos.appendChild(paragraph);
-    input.value = '';
+class item{
+    constructor(itemName) {
+        this.createDiv(itemName);
+    }
+
+    createDiv(itemName){
+
+        let input = document.createElement('input');
+        input.value = itemName;
+        input.disabled = true;
+        input.classList.add('item_input');
+        input.type = 'text';
+
+        let itemBox = document.createElement('div');
+        itemBox.classList.add('item');
+
+        let editButton = document.createElement('button');
+        editButton.innerHTML = "EDIT" ; 
+        editButton.classList.add('editButton');
+
+        let removeButton = document.createElement('button');
+        removeButton.innerHTML = "REMOVE" ;
+        removeButton.classList.add('removeButton');
+
+        container.appendChild(itemBox);
+        itemBox.appendChild(input);
+        itemBox.appendChild(editButton);
+        itemBox.appendChild(removeButton);
+
+        editButton.addEventListener('click',() => this.edit(input));
+        removeButton.addEventListener('click', () => this.remove(itemBox));
+    }
+
+    edit(input){
+        input.disabled = !input.disabled;
+    }
+
+    remove(item){
+        container.removeChild(item);
+    }
+
+}
+
+function check(){
+    if(input.value != ""){
+        new item(input.value);
+        input.value = "";
+    }
+}
+
+addButton.addEventListener('click', check);
+window.addEventListener('keydown', (e) =>{
+    if (e.which == 13){
+        check();
+    }
 })
